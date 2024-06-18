199ff52dceabd05a758a6efa38b685f79875a905
EdwinVBN <Katuku.edwin@gmail.com>
2024-06-18 16:36:06 +0200

    Update Home.php

--- Frontend/Home.php
+++ Frontend/Home.php
@@ -1,3 +1,8 @@
+<?php
+    require_once('../classes/Class.fetchpost.php');
+    $posts = new FetchPosts();
+    $allPost = $posts->fetchPost();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +13,11 @@
 <body>
     <header>
         <a href="./login.php">Login</a>
+        <h1>Overzicht</h1>
     </header>
-    home
+    <section>
+
+    </section>
+    
 </body>
 </html>
